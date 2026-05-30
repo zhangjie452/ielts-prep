ca4923aebc21942653a124006c20a5f037c45227
lvdian <lvdian@users.noreply.github.com>
2026-05-30 15:03:37 +0800

    fix: remove crossorigin attribute to fix Vercel module loading

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-﻿import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'remove-crossorigin',
+      transformIndexHtml(html) {
+        return html.replaceAll('crossorigin ', '')
+      }
+    }
+  ],
   base: '/',
   build: {
     outDir: 'dist',
